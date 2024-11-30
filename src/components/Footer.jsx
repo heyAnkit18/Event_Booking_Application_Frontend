fb2df2ee368405054ca58975f9e23eedab1d019a
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-12-01 00:17:50 +0530

    Footer.jsx Added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="banner">
+        <div className="footer-title">
+          <h1>EventBliss</h1>
+          <p>Your Dream Events, Perfectly Planned</p>
+        </div>
+        <div className="newsletter">
+          <h3>Stay Updated!</h3>
+          <p>Sign up with your email address to receive the latest news and updates.</p>
+          <div className="newsletter-input">
+            <input type="email" placeholder="Enter your email" />
+            <button>Subscribe</button>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} EventBliss. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
