d07bfe3baaf54c0f66906d10ad14f4d5a5621e4e
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-11-30 14:32:13 +0530

    About.jsx Added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className="about container">
+      <h2>ABOUT</h2>
+      <p>
+        Welcome to EventBliss, your partner in creating unforgettable experiences. With our team of seasoned professionals, we specialize in crafting events that surpass expectations. From intimate gatherings to large-scale celebrations, we meticulously handle every detail, ensuring seamless execution and unparalleled excellence.
+      </p>
+      <p>
+        At Events.com, we understand that each event is unique. That's why we take a personalized approach, working closely with you to understand your vision, preferences, and objectives. Whether you're planning a corporate function, a wedding, or a social soirée, we tailor our services to meet your specific needs and exceed your wildest dreams.
+      </p>
+      <p>
+        Our expertise spans every aspect of event planning, including venue selection, theme development, décor, catering, entertainment, logistics, and more. Whether you require full-service event management or assistance with specific elements, our dedicated team is here to guide you every step of the way, ensuring a flawless and unforgettable experience for you.
+      </p>
+      <p>
+        We believe that every event is an opportunity to create lasting memories. Our commitment to excellence and passion for detail ensures that every moment, from the first step to the final toast, is meticulously planned and executed. No matter the size or scale, EventBliss is here to make your event extraordinary.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
