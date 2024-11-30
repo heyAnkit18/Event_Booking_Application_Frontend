8fdf98b6cd42dcdf4de30d5c3f189c015a9db1b9
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-11-30 12:59:31 +0530

    Navbar.css Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-scroll";
+import "./Navbar.css"; 
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <div className="logo">MomentFlow</div>
+      <div className="navLinks">
+        <div className="links">
+          <Link to="hero" spy={true} smooth={true} duration={500}>
+            HOME
+          </Link>
+          <Link to="services" spy={true} smooth={true} duration={500}>
+            SERVICES
+          </Link>
+          <Link to="about" spy={true} smooth={true} duration={500}>
+            ABOUT
+          </Link>
+          <Link to="contact" spy={true} smooth={true} duration={500}>
+            CONTACT
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
