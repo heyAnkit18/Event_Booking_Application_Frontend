e08bb1e20093d19095a6a46df8dfee44d373f7f6
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-11-30 13:46:07 +0530

    HeroSection.jsx Added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-scroll";
+import "./HeroSection.css";
+import restaurantImage from "../assets/restaurant.jpg";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
-}
+    <section className="hero">
+      <div className="hero-overlay"></div> 
+      <img className="hero-image" src={restaurantImage} alt="restaurant" />
+      <div className="hero-content">
+        <h3>TIME TO PARTY</h3>
+        <h1>THE PERFECT VENUE FOR YOUR NEXT EVENT</h1>
+        <p>
+          We believe that it is all about the <span className="highlight">BIG DREAMS</span> and <span className="highlight">BIG CHEERS</span>!!!
+        </p>
+        <Link to="contact" spy={true} smooth={true} duration={500} className="btn">
+          BOOK NOW
+        </Link>
+      </div>
+    </section>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
+
